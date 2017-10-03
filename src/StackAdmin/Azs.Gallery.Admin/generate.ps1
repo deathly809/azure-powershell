f215d50314a235297f092707e1c1658e48dec645
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2017-10-02 18:22:36 -0700

    Working

---
+++ src/StackAdmin/Azs.Gallery.Admin/generate.ps1
@@ -0,0 +1,6 @@
+
+$rpName = "Gallery"
+$location = pwd
+$psswagger = "E:\github\PSswagger"
+$module = "Module"
+. ..\..\..\tools\GeneratePSSwagger.ps1 -RPName $rpName -Location $location -Admin -ModuleDirectory $module -AzureStack -PSSwaggerLocation $psswagger
