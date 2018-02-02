7b78b4e93c8711cebcefacbe6e7c18ea25c7708b
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-02-02 08:18:44 -0800

    Generated Module

---
+++ src/StackAdmin/Azs.Commerce.Admin/TestModule/Azs.Commerce.Admin/Generated.PowerShell.Commands/SwaggerPathCommands/Update-Encryption.ps1
@@ -0,0 +1,76 @@
+<#
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License. See License.txt in the project root for license information.
+#>
+
+<#
+.SYNOPSIS
+    
+
+.DESCRIPTION
+    Update the encryption.
+
+#>
+function Update-Encryption
+{
+    [CmdletBinding(DefaultParameterSetName='UpdateEncryption')]
+    param(
+    )
+
+    Begin 
+    {
+	    Initialize-PSSwaggerDependencies -Azure
+        $tracerObject = $null
+        if (('continue' -eq $DebugPreference) -or ('inquire' -eq $DebugPreference)) {
+            $oldDebugPreference = $global:DebugPreference
+			$global:DebugPreference = "continue"
+            $tracerObject = New-PSSwaggerClientTracing
+            Register-PSSwaggerClientTracing -TracerObject $tracerObject
+        }
+	}
+
+    Process {
+    
+    $ErrorActionPreference = 'Stop'
+
+    $NewServiceClient_params = @{
+        FullClientTypeName = 'Microsoft.AzureStack.Management.Commerce.Admin.CommerceAdminClient'
+    }
+
+    $GlobalParameterHashtable = @{}
+    $NewServiceClient_params['GlobalParameterHashtable'] = $GlobalParameterHashtable
+     
+    $GlobalParameterHashtable['SubscriptionId'] = $null
+    if($PSBoundParameters.ContainsKey('SubscriptionId')) {
+        $GlobalParameterHashtable['SubscriptionId'] = $PSBoundParameters['SubscriptionId']
+    }
+
+    $CommerceAdminClient = New-ServiceClient @NewServiceClient_params
+
+
+    if ('UpdateEncryption' -eq $PsCmdlet.ParameterSetName) {
+        Write-Verbose -Message 'Performing operation UpdateEncryptionWithHttpMessagesAsync on $CommerceAdminClient.'
+        $TaskResult = $CommerceAdminClient.UpdateEncryptionWithHttpMessagesAsync()
+    } else {
+        Write-Verbose -Message 'Failed to map parameter set to operation method.'
+        throw 'Module failed to find operation to execute.'
+    }
+
+    if ($TaskResult) {
+        $GetTaskResult_params = @{
+            TaskResult = $TaskResult
+        }
+            
+        Get-TaskResult @GetTaskResult_params
+        
+    }
+    }
+
+    End {
+        if ($tracerObject) {
+            $global:DebugPreference = $oldDebugPreference
+            Unregister-PSSwaggerClientTracing -TracerObject $tracerObject
+        }
+    }
+}
+
