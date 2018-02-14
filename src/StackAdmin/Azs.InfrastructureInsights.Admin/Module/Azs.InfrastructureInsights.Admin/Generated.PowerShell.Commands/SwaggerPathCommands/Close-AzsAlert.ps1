b9eb063a7447011611d910125f330f232787c3b2
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-02-14 13:24:15 -0800

    Fixed Close-AzsAlert

--- src/StackAdmin/Azs.InfrastructureInsights.Admin/Module/Azs.InfrastructureInsights.Admin/Generated.PowerShell.Commands/SwaggerPathCommands/Close-AzsAlert.ps1
+++ src/StackAdmin/Azs.InfrastructureInsights.Admin/Module/Azs.InfrastructureInsights.Admin/Generated.PowerShell.Commands/SwaggerPathCommands/Close-AzsAlert.ps1
@@ -10,65 +10,108 @@ Licensed under the MIT License. See License.txt in the project root for license 
 .DESCRIPTION
     Closes the given alert.
 
+.PARAMETER Description
+    Description of the alert.
+
+.PARAMETER Remediation
+    Admin friendly remediation instructions for the alert.
+
+.PARAMETER FaultId
+    Fault id of the alert.
+
+.PARAMETER ResourceRegistrationId
+    Registration id of the atomic component the alert belongs to.  This is null if not associated with a resource.
+
 .PARAMETER InputObject
     The input object of type Microsoft.AzureStack.Management.InfrastructureInsights.Admin.Models.Alert.
+
+.PARAMETER Location
+    Location where resource is location.
+
+.PARAMETER LastUpdatedTimestamp
+    Timestamp when the alert was last updated.
+
+.PARAMETER User
+    The username used to perform the operation.
+
+.PARAMETER AlertProperties
+    Properties of the alert.
+
+.PARAMETER Severity
+    Severity of the alert.
+
+.PARAMETER State
+    State of the alert.
+
+.PARAMETER Title
+    Title of the alert.
+
+.PARAMETER ClosedByUserAlias
+    User alias who closed the alert.
 
 .PARAMETER ResourceId
     The resource id.
 
+.PARAMETER ImpactedResourceDisplayName
+    Display name for the impacted item.
+
+.PARAMETER FaultTypeId
+    Fault type id of the alert.
+
 .PARAMETER Region
     Name of the region
+
+.PARAMETER CreatedTimestamp
+    Timestamp when the alert was created.
+
+.PARAMETER Id
+    URI of the resource.
 
 .PARAMETER ResourceGroupName
     resourceGroupName.
 
-.PARAMETER Alert
-    Updated Alert Parameter.
-
 .PARAMETER Name
     Name of the alert.
 
-.PARAMETER User
-    The username used to perform the operation.
+.PARAMETER Tags
+    List of key value pairs.
+
+.PARAMETER Type
+    Type of resource.
+
+.PARAMETER ImpactedResourceId
+    ResourceId for the impacted item.
+
+.PARAMETER ResourceProviderRegistrationId
+    Registration id of the service the alert belongs to.
+
+.PARAMETER Name
+    Name of the resource.
+
+.PARAMETER AlertId
+    Id of the alert.
+
+.PARAMETER ClosedTimestamp
+    Timestamp when the alert was closed.
 
 #>
 function Close-AzsAlert
 {
     [OutputType([Microsoft.AzureStack.Management.InfrastructureInsights.Admin.Models.Alert])]
-    [CmdletBinding(DefaultParameterSetName='Alerts_Close')]
     param(    
+    
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'InputObject_Alerts_Close')]
         [Microsoft.AzureStack.Management.InfrastructureInsights.Admin.Models.Alert]
         $InputObject,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'InputObject_Alerts_Close')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'ResourceId_Alerts_Close')]
+        [System.String]
+        $User,
     
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ResourceId_Alerts_Close')]
         [System.String]
-        $ResourceId,
-    
-        [Parameter(Mandatory = $true, ParameterSetName = 'Alerts_Close')]
-        [System.String]
-        $Region,
-    
-        [Parameter(Mandatory = $true, ParameterSetName = 'Alerts_Close')]
-        [System.String]
-        $ResourceGroupName,
-    
-        [Parameter(Mandatory = $true, ParameterSetName = 'InputObject_Alerts_Close')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'ResourceId_Alerts_Close')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'Alerts_Close')]
-        [Microsoft.AzureStack.Management.InfrastructureInsights.Admin.Models.Alert]
-        $Alert,
-    
-        [Parameter(Mandatory = $true, ParameterSetName = 'Alerts_Close')]
-        [Alias('AlertName')]
-        [System.String]
-        $Name,
-    
-        [Parameter(Mandatory = $true, ParameterSetName = 'InputObject_Alerts_Close')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'ResourceId_Alerts_Close')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'Alerts_Close')]
-        [System.String]
-        $User
+        $ResourceId
     )
 
     Begin 
@@ -100,9 +143,6 @@ function Close-AzsAlert
     }
 
     $InfrastructureInsightsAdminClient = New-ServiceClient @NewServiceClient_params
-
-    $AlertName = $Name
-
  
     if('InputObject_Alerts_Close' -eq $PsCmdlet.ParameterSetName -or 'ResourceId_Alerts_Close' -eq $PsCmdlet.ParameterSetName) {
         $GetArmResourceIdParameterValue_params = @{
@@ -111,16 +151,24 @@ function Close-AzsAlert
 
         if('ResourceId_Alerts_Close' -eq $PsCmdlet.ParameterSetName) {
             $GetArmResourceIdParameterValue_params['Id'] = $ResourceId
+            $Alert = Get-AzsAlert -ResourceId $ResourceId
         }
         else {
             $GetArmResourceIdParameterValue_params['Id'] = $InputObject.Id
+            $Alert = $InputObject
         }
+
         $ArmResourceIdParameterValues = Get-ArmResourceIdParameterValue @GetArmResourceIdParameterValue_params
         $resourceGroupName = $ArmResourceIdParameterValues['resourceGroupName']
 
         $region = $ArmResourceIdParameterValues['region']
 
         $alertName = $ArmResourceIdParameterValues['alertName']
+
+        if(-not $User) {
+            $ctx = Get-AzureRmContext
+            $User = $ctx.Account.Id
+        }
     }
 
 
