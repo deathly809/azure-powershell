d989482fbd74055181ce5438989859e98d772e97
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-02-23 09:02:37 -0800

    Removed other moduels

--- src/StackAdmin/Azs.Commerce.Admin/generate.ps1
+++
@@ -1,6 +0,0 @@
-
-$rpName = "commerce"
-$location = pwd
-$moduleName = "CommerceAdmin"
-
-. ..\..\..\tools\GeneratePSSwagger.ps1 -RPName $rpName -Location $location -Name $moduleName -Admin
--- src/StackAdmin/Azs.InfrastructureInsights.Admin/generate.ps1
+++
@@ -1,6 +0,0 @@
-
-$rpName = "InfrastructureInsights"
-$location = pwd
-$psswagger = "E:\github\PSswagger"
-$module = "Module"
-. ..\..\..\tools\GeneratePSSwagger.ps1 -RPName $rpName -Location $location -Admin -ModuleDirectory $module -AzureStack -PSSwaggerLocation $psswagger
